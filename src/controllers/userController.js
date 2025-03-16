0facc7ebe02f1677669c060dc74b6f1d6c26d962
Mufid Alnadi <126170946+MufidAlnadi@users.noreply.github.com>
2025-03-16 18:47:03 +0300

    Update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const {body, validationResult} = require('express-validator');
 const User = require('../models/user');
 const logger = require('../config/logger');
-
+const sequelize = require('../config/database');
 // Get All Users
 const getAllUsers = async (req, res) => {
   try {
@@ -39,10 +39,11 @@ const getUserById = async (req, res) => {
 };
 
 // Update User Balance
+
 const updateBalance = [
   body('userId').isUUID().withMessage('Invalid user ID'),
   body('amount')
-    .isFloat({min: -99999999}) 
+    .isFloat({min: -99999999})
     .withMessage('Amount must be a valid number')
     .notEmpty()
     .withMessage('Amount cannot be empty'),
@@ -55,33 +56,40 @@ const updateBalance = [
 
     const {userId, amount} = req.body;
 
-    try {
-     
-      const parsedAmount = parseFloat(amount);
+    const transaction = await sequelize.transaction();
 
+    try {
+      const parsedAmount = parseFloat(amount);
       if (isNaN(parsedAmount)) {
         return res.status(400).json({message: 'Amount must be a valid number'});
       }
 
-      const user = await User.findByPk(userId);
+      const user = await User.findOne({
+        where: {id: userId},
+        lock: true,
+        transaction,
+      });
 
       if (!user) {
         return res.status(404).json({message: 'User not found'});
       }
 
-      const currentBalance = parseFloat(user.balance); 
+      const currentBalance = parseFloat(user.balance);
       if (currentBalance + parsedAmount < 0) {
         return res.status(400).json({message: 'Insufficient funds'});
       }
 
       user.balance = currentBalance + parsedAmount;
-      await user.save();
+      await user.save({transaction});
+
+      await transaction.commit();
 
       return res.status(200).json({
         message: 'Balance updated successfully.',
         balance: user.balance,
       });
     } catch (error) {
+      await transaction.rollback();
       logger.error('Error updating balance:', error);
       return res.status(500).json({message: 'Internal Server Error'});
     }
